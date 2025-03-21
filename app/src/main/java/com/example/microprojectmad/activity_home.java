fb9fa3d135974ba06c56b9e79073380776d225bb
Aniket Aher <aniketaher2002@gmail.com>
2025-03-22 00:09:00 +0530

    home page design

---
+++ app/src/main/java/com/example/microprojectmad/activity_home.java
@@ -0,0 +1,35 @@
+package com.example.microprojectmad;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class activity_home extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ImageButton imageButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+
+        drawerLayout = findViewById(R.id.drawablelayout);
+        imageButton = findViewById(R.id.imagebtn);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.open();
+            }
+        });
+
+    }
+}
