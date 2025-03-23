f4c7e3531974d047328a2d4221bc92f285e3e519
om <aniketaher2002@gmail.com>
2025-03-23 15:44:24 +0530

    bottom navigation done

--- app/src/main/java/com/example/microprojectmad/Login.java
+++ app/src/main/java/com/example/microprojectmad/Login.java
@@ -31,7 +31,7 @@ public class Login extends AppCompatActivity {
         registerlink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this , activity_itemdetail.class);
+                Intent intent = new Intent(Login.this , activity_home.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/microprojectmad/activity_home.java
+++ app/src/main/java/com/example/microprojectmad/activity_home.java
@@ -2,20 +2,30 @@ package com.example.microprojectmad;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class activity_home extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ImageButton imageButton,notification;
+    FrameLayout frameLayout;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +35,8 @@ public class activity_home extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawablelayout);
         imageButton = findViewById(R.id.imagebtn);
         notification = findViewById(R.id.notification);
+        frameLayout = findViewById(R.id.framelayout);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +52,49 @@ public class activity_home extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if(itemId == R.id.home){
+                    loadFragment(new HomeFragment(),false);
+                    
+                } else if (itemId == R.id.report) {
+                    loadFragment(new ReportFragment(),false);
+                    
+                } else if (itemId == R.id.notification) {
+                    loadFragment(new NotificationFragment(),false);
+                }
+                else {
+                    loadFragment(new ProfileFragment(),false);
+                }
+
+
+
+                return true;
+            }
+        });
+
+        loadFragment(new HomeFragment(),true);
+
+
+
+    }
+    public void loadFragment(Fragment fragment, boolean isAppInitialized){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if(isAppInitialized){
+            fragmentTransaction.add(R.id.framelayout,fragment);
+        }else {
+            fragmentTransaction.replace(R.id.framelayout, fragment);
+        }
+
+        fragmentTransaction.commit();
+
 
     }
 }
